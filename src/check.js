6d49005325e3bd9281f45d42b8b07c8e9bb6b77a
James Rodovich <jim@inventables.com>
2015-10-23 16:38:23 -0500

    Rephrase expression statements as if statements with logging.

--- src/check.js
+++ src/check.js
@@ -6,42 +6,42 @@ var check = function() {
     throw new Error('Expected function as last argument to `check`, got ' + (typeof checker));
   }
   var values = [].slice.call(arguments, 0, arguments.length - 1);
-  var parsed = esprima.parse('(' + checker.toString() + ');');
-  var funcExpression = parsed.body[0].expression;
-  var funcParams = funcExpression.params;
-  var funcStatements = funcExpression.body.body;
-  funcStatements.forEach(checkStatement);
+
+  var parserInput = '(' + checker.toString() + ');';
+  var checkerFunctionExpression = esprima.parse(parserInput, { range: true }).body[0].expression;
+  var checkerStatements = checkerFunctionExpression.body.body;
+
+  var rephraseAsCheckWithLogging = function(statement) {
+    var expression = getExpression(statement);
+    if (expression) {
+      var expressionString = parserInput.substring(expression.range[0], expression.range[1]);
+      return [
+        'if (' + expressionString + ') {',
+        '  console.log("Yay! " + ' + JSON.stringify(expressionString) + ');',
+        '} else {',
+        '  console.log("Oops! Expected " + ' + JSON.stringify(expressionString) + ');',
+        '}'
+      ].join('\n');
+    }
+  };
+
+  checker = eval([].concat(
+    '(function (' + checkerFunctionExpression.params.map(function(param) { return param.name }).join(', ') + ') {',
+    checkerStatements.map(rephraseAsCheckWithLogging),
+    '})'
+  ).join('\n'));
+  checker.apply(this, values);
 };
 
-var checkStatement = function(statement) {
+var getExpression = function(statement) {
   switch (statement.type) {
     case 'ExpressionStatement':
-      checkExpression(statement.expression);
-      break;
+      return statement.expression;
+    case 'ReturnStatement':
+      return statement.argument;
     default:
       logApathy(statement.type);
   }
-};
-
-var checkExpression = function(expression) {
-  switch (expression.type) {
-    case 'Identifier':
-      logHope(JSON.stringify(expression.name));
-      break;
-    case 'BinaryExpression':
-      logHope(JSON.stringify(expression.left) + ' ' + expression.operator + ' ' + JSON.stringify(expression.right));
-      break;
-    default:
-      logApathy(expression.type);
-  }
-};
-
-var logHope = function(hopedCondition) {
-  console.log('I sure hope ' + hopedCondition + '!!');
-};
-
-var logApathy = function(apatheticFactor) {
-  console.log('I don\'t understand your ' + apatheticFactor + ' and I won\'t enforce it.');
 };
 
 module.exports = check;
